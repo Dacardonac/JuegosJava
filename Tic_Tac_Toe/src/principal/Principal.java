46abfa4c7d5188e289110c017251ac4a55f58285
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2026-04-29 23:08:12 -0500

    chore: Inicializando el proyecto (menú del juego).

---
+++ Tic_Tac_Toe/src/principal/Principal.java
@@ -0,0 +1,12 @@
+// @author Daniel A. Cano
+package principal;
+
+import vista.FormInicio;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FormInicio formInicio = new FormInicio();
+        formInicio.setVisible(true);
+    }
+}
